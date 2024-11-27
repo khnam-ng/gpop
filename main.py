69b5882776c602b9f104fa0d1efcb82c3257f280
smivys <khanhnamxz@hotmail.com>
2024-11-27 03:27:49 +0100

    finished task 1 and task 2, forgot to submit for task 1

--- main.py
+++ main.py
@@ -1,32 +1,112 @@
 from nicegui import ui
-import matplotlib.pyplot as plt
+import numpy as np
 from tasker import Tasker
 
 tasker = Tasker()
+simulations = 1000
+N = 100
+mutation_rate = 0.1
 
 intro = ui.markdown('''
-    # **GPOP** 🗣️🗣️
-
-    
+    # **GPOP 05/12/2024** 🗣️🗣️
 ''')
+def task1(p, N):
+    with ui.row() as task1_row:
+        with ui.card().classes('w-[1300px] mx-auto'):
+            ui.markdown('## Genetic Drift')
+            with ui.matplotlib(figsize=(12, 6)).figure as fig:
+                ax = fig.gca()
+                for i in range(5):
+                    gen_drift = tasker.task_1(p=p, N=N)
+                    ax.plot(gen_drift, label=f"pop {i}")
+                ax.set_ylim(ymin=0, ymax=1)
+                ax.set_xlim(xmin=0, xmax=2*N)
+                ax.set_xlabel('Generations')
+                ax.set_ylabel('p: frequency of allele A')
+                ax.legend()
+        with ui.card().classes('w-[500px] mx-auto'):
+            ui.markdown('##### Cheatsheet')
+            ui.button('mr. clean', on_click=lambda: (task1_row.delete()))
+            fixations = 0
+            times = []            
+            for i in range(simulations):
+                gen_drift = tasker.task_1(p=p, N=N)
+                if gen_drift[-1] == 1 or 0:
+                    fixations += 1
+                times.append(len(gen_drift) - 1)
+            ui.markdown(f'The fixation probability of allele A with pA=**{p}** over {simulations} simulations:\
+                         **{round(fixations/simulations, 2)}**')
+            ui.markdown("""Fixation probability of allele A in pop = initial allele frequency pA.                        
+                        For pA = 0.2, fixation probability of allele A is in range(0.12, 0.17)""")
+            ui.markdown(f'The expected fixation time (number of generations): {round(np.mean(times), 2)}')
+            ui.markdown(r'''
+                            Harmonic mean of actual pop size:
+                            $$Ne = (\frac{1}{t}\sum_{i=0}^{t-1} \frac{1}{Ni})^-1$$
+                        ''', extras=['latex'])
+            ui.markdown('Theoretically, with N = 100 and generations = 200, Ne ~ 100')
+            
+def task2(N):
+    with ui.row() as task_row:
+        with ui.card().classes('w-[1300px] mx-auto'):
+            ui.markdown('## Coalescent model')
+            with ui.matplotlib(figsize=(12, 6)).figure as fig:
+                ax = fig.gca()
+                generation, freq = tasker.task_2_identicalByDescent(N = N)
+                ax.plot(freq, label='frequency of an allele from 1/N to 1')
+                ax.set_ylim(ymin=0, ymax=1)
+                ax.set_xlim(xmin=0, xmax=2*N)
+                ax.set_xlabel('Generations')
+                ax.set_ylabel('Frequency of an allele')
+                ax.legend()
+                ax.set_title('All alleles are identical by descent')
+            with ui.matplotlib(figsize=(12, 6)).figure as fig2:
+                sample_size = [2, 3, 4, 5]
+                E_Tn = []
+                generations_arr = []
+                for n in sample_size:
+                    E_Tn.append(2 * N / (n * (n - 1)))
+                    generationsForEachN = []
+                    for i in range(simulations):
+                        generationsForEachN.append(tasker.task_2_firstCoalescentEvent(n=n, N=N))
+                    generations_arr.append(np.mean(generationsForEachN))
+                ax = fig2.gca()
+                ax.plot(sample_size, E_Tn, label='Theoretical', marker='o', color='green')
+                ax.plot(sample_size, generations_arr, label='Empirical', marker='x', color='red')
+                ax.set_xlabel("Sample size n = [2, 3, 4, 5]")
+                ax.set_ylabel("Generations to First Coalescent Event")
+                ax.set_title("Empirical / Theoretical")
+                ax.legend()
+        with ui.card().classes('w-[500px] mx-auto'):
+            ui.markdown('##### Cheatsheet')
+            ui.button('mr. clean', on_click=lambda: (task_row.delete()))
+            ui.markdown('Tracking a population of N = 100 individuals until all alleles are identical by descent\
+                        is like tracking genetic drift.')
+            ui.markdown('A population has N alleles with first frequency of each allele = 1/N. \
+                        To count generations until frequency of an allele reach 1. \
+                        That means this population has 1 allele left.')
+            ui.markdown(r'''
+                            Expected generations to the first coalescent event:
+                            $$E\{Tn\} = \frac{4N}{n(n-1)}$$
+                            $$E\{T2\} = 100  $$
+                            $$E\{T3\} = 33.34$$
+                            $$E\{T4\} = 16.67$$
+                            $$E\{T5\} = 10   $$
+                        ''', extras=['latex'])
+            
 with ui.row():
-    ui.button('Task 1', on_click=lambda: (tasker.task_1(p=1, N=100), card_label.set_content('## Genetic Drift')))
-    ui.button('Task 2', on_click=lambda: (tasker.task_2(p=1), card_label.set_content('## Coalescent model')))
-    ui.button('Task 3', on_click=lambda: (tasker.task_3(p=1, N=100, mutation_rate=0.1), card_label.set_content('## Mutations')))
-    ui.button('Task 4', on_click=lambda: (tasker.task_4(), card_label.set_content('## Selection')))
-    ui.button('Task 5', on_click=lambda: (tasker.task_5(), card_label.set_content('## Clonal inference')))
-    ui.button('Task 6', on_click=lambda: (tasker.task_6(), card_label.set_content('## Population structure')))
-    ui.button('Task 7', on_click=lambda: (tasker.task_7(), card_label.set_content('## Migration')))
+    ui.button('Task 1', on_click=lambda: (task1(p=0.2, N=N)))
+            # badge1 = ui.badge('!', color='red').props('floating')
+        
+    ui.button('Task 2', on_click=lambda: (task2(N=N)))
+        # badge2 = ui.badge('!', color='red').props('floating')
+    # ui.button('Task 3', on_click=lambda: (tasker.task_3(p=1, N=100, mutation_rate=0.1), card_label.set_content('## Mutations')))
+    # ui.button('Task 4', on_click=lambda: (tasker.task_4(), card_label.set_content('## Selection')))
+    # ui.button('Task 5', on_click=lambda: (tasker.task_5(), card_label.set_content('## Clonal inference')))
+    # ui.button('Task 6', on_click=lambda: (tasker.task_6(), card_label.set_content('## Population structure')))
+    # ui.button('Task 7', on_click=lambda: (tasker.task_7(), card_label.set_content('## Migration')))
 
-with ui.row():
-    with ui.card().classes('w-[1300px] h-[600px] mx-auto'):
-        card_label = ui.markdown('')
-    with ui.card().classes('w-[500px] h-[600px] mx-auto'):
-        ui.markdown('##### Cheatsheet')
-        ui.markdown('''''')
-
-url = 'https://github.com/zauberzeug/nicegui/'
-ui.button('Open GitHub', on_click=lambda: ui.navigate.to(url, new_tab=True))
+# url = 'https://github.com/zauberzeug/nicegui/'
+# ui.button('Open GitHub', on_click=lambda: ui.navigate.to(url, new_tab=True))
 
 ui.dark_mode().enable()
 ui.run(title='give me 20', favicon='🗣️')
--- tasker.py
+++ tasker.py
@@ -1,16 +1,54 @@
 import numpy as np
-# import matplotlib.pyplot as plt
 
 class Tasker():
     def __init__(self):
         pass
-    
+
     def task_1(self, p, N):
         print('Task 1: Genetic drift')
-        q = 1 - p
+        population = np.random.choice(['A', 'B'], size=N, p=[p, 1 - p])
+        generations = 2*N #each new generation is obtained from the previous generation 2N times
+        allele_freqs = []
 
-    def task_2(self, p, N = 100):
-        print('Task 2: Coalescent model')
+        for gen in range(generations): 
+            allele_freq_A = np.sum(population == 'A') / N #Pt depends on Pt-1
+            allele_freqs.append(allele_freq_A)
+            
+            #choosing an allele at random
+            #Each individual in generation t+1 is a copy of a randomly selected individual in generation t.
+            population = np.random.choice(population, size=N, replace=True)
+            
+            #fixation
+            if allele_freq_A == 1 or allele_freq_A == 0:
+                break
+        return allele_freqs
+                              
+    def task_2_identicalByDescent(self, N):
+        print('Task 2: Coalescent model + identical by descent')
+        population = np.arange(N)
+        generations = 0
+        freq = []
+        freq.append(1/N) #population has N alleles then first frequency of each allele = 1/N
+
+        while len(set(population)) > 1:  #unique number in population, stop when population has only one number left.
+            generations += 1
+            #Each individual in generation t+1 is a copy of a randomly selected individual in generation t.
+            population = np.random.choice(population, size=N, replace=True)
+            #If True, also return the number of times each unique item appears in ar.
+            unique, counts = np.unique(population, return_counts=True)
+            freq.append(np.max(counts) / N)
+        
+        return generations, freq
+
+    def task_2_firstCoalescentEvent(self, n, N):
+        print('Task 2: Coalescent model + sample size')
+        generations = 0
+        while 1:
+            generations += 1
+            coalescent_proba = n*(n-1)/(2*N)
+            if np.random.rand() < coalescent_proba:
+                break
+        return generations
 
     def task_3(self, p, N, mutation_rate):
         print('Task 3: Mutations in the infinite-allele model')
@@ -20,7 +58,7 @@ class Tasker():
         # or mutations between parent and offspring may appear with some mutation rate.
         print('Task 4: Selection')
     
-    def task_5(self, pA = 0.79, pB = 0.2, pC = 0.01, N = 100):
+    def task_5(self, N, pA = 0.79, pB = 0.2, pC = 0.01):
         print('Task 5: Clonal inference')
     
     def task_6(self):
